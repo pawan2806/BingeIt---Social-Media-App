01156cee8e4a976420d15404fefe59939d5488f9
Pawan <pawan.singh.co19@nsut.ac.in>
2021-03-13 01:56:17 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:buddiesgram/pages/HomePage.dart';
+import 'package:flutter/material.dart';
+
+void main()
+{
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BuddiesGram',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData
+      (
+        scaffoldBackgroundColor: Colors.black,
+        dialogBackgroundColor: Colors.black,
+        primarySwatch: Colors.grey,
+        accentColor: Colors.black,
+        cardColor: Colors.white70,
+
+      ),
+      home: HomePage(),
+    );
+  }
+}
